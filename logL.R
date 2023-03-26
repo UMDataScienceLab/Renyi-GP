33b46881a8752e8710be154869cb66c5eddca189
UofM Data Science Lab <alkontar@umich.edu>
2023-03-26 16:22:55 -0400

    Update logL.R

--- logL.R
+++ logL.R
@@ -40,26 +40,26 @@ logL=function(H,fn)
   
   ## implementation of Eq. (7) in the main paper
   
-  r_cuu <- cuu(z, z, H); r_cfu <- cfu(x, z, H); r_cff <- cff(x, x, H)
-  fuf <- r_cfu%*%mBCG_noT(r_cuu, t(r_cfu), maxiter = MAT_ITER)
-  B <- H[6] * fuf + diag(H[2*n+2]^2, dim(fuf)[1]) + (1-H[6]) * r_cff
+  r_cuu <- cuu(z, z, H); r_cfu <- cfu(x, z, H); r_cff <- cff(x, x, H) # calculate covariance matrices
+  fuf <- r_cfu%*%mBCG_noT(r_cuu, t(r_cfu), maxiter = MAT_ITER) # calculate low-rank approximation matrix Q
+  B <- H[6] * fuf + diag(H[2*n+2]^2, dim(fuf)[1]) + (1-H[6]) * r_cff # calculate the covariance of alpha-ELBO objective
   
   RHS <- cbind(y, r_cfu)
   
-  B_inv_y <- mBCG_noT(B, RHS, maxiter = MAT_ITER)
+  B_inv_y <- mBCG_noT(B, RHS, maxiter = MAT_ITER) # run conjugate method for efficient matrix product
   
-  A <- diag(H[2*n+2]^2, dim(fuf)[1]) + (1-H[6]) * r_cff + H[6] * fuf
-  ch <- chol(A + diag(0, ncol(A) ), pivot=TRUE, tol=1e-16)
-  logdeter_A <- 2*(sum(log(diag(ch))))
+  A <- diag(H[2*n+2]^2, dim(fuf)[1]) + (1-H[6]) * r_cff + H[6] * fuf # calculate matrix \xi in Eq. (6) defined in Appendix C
+  ch <- chol(A + diag(0, ncol(A) ), pivot=TRUE, tol=1e-16) # cholesky decomposition 
+  logdeter_A <- 2*(sum(log(diag(ch)))) # calculate log-determinant of \xi
   
   # logdeter <- log(det(  diag(1, ncol(r_cuu)) + H[6]* mBCG_noT(r_cuu, t(r_cfu), maxiter = MAT_ITER)%*%RHS[,2:(ncol(RHS))]  )) + logdeter_A
   logdeter <- logdeter_A
   
-  B2 <-  diag(1, ncol(r_cuu)) + (1-2*H[6]) * mBCG_noT(r_cuu, t(r_cfu), maxiter = MAT_ITER)%*%B_inv_y[,2:(ncol(RHS))]
-
-  logdeter2 <- log(det(B2))
+  B2 <-  diag(1, ncol(r_cuu)) + (1-2*H[6]) * mBCG_noT(r_cuu, t(r_cfu), maxiter = MAT_ITER)%*%B_inv_y[,2:(ncol(RHS))] # calculate C_x in Appendix C
+ 
+  logdeter2 <- log(det(B2)) # calculate the log determinant
   
-  # likelihood
+  # return alpha-ELBO objective
   a <- -0.5*log(2*pi)*length(y) + 
     (-1/(2-2*H[6]))*logdeter - 
     0.5*t(y)%*%B_inv_y[,1] + 
